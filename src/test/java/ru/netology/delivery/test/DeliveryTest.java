2d42bc47a2f32a52ae487773450c69886d3266e4
YuliyaTonkikh <uliya_i@bk.ru>
2023-04-17 21:24:39 +0700

    fix based on comments

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -22,6 +22,34 @@ class DeliveryTest {
     }
 
     @Test
+    void shouldSuccessfulPlanAndReplanMeeting() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = DataGenerator.generateRandomDateShift();
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = DataGenerator.generateRandomDateShift();
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+
+        $("[data-test-id='city']").$("[placeholder='Город']").setValue(validUser.getCity());
+        $("[data-test-id='date']").$("[placeholder='Дата встречи']").setValue(firstMeetingDate);
+        $("[data-test-id= 'name']").$("[name ='name']").setValue(validUser.getName());
+        $("[data-test-id='phone']").$("[name='phone']").setValue(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id='success-notification']").shouldBe(visible).
+                shouldHave(exactText("Успешно!\n" + "Встреча успешно запланирована на " + firstMeetingDate));
+        setup();
+        $("[data-test-id='city']").$("[placeholder='Город']").setValue(validUser.getCity());
+        $("[data-test-id='date']").$("[placeholder='Дата встречи']").setValue(secondMeetingDate);
+        $("[data-test-id= 'name']").$("[name ='name']").setValue(validUser.getName());
+        $("[data-test-id='phone']").$("[name='phone']").setValue(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id= 'replan-notification']").shouldHave(exactText("Необходимо подтверждение\n" + "У вас уже запланирована встреча на другую дату. Перепланировать?\n" + "Перепланировать"));
+        $$("button").find(exactText("Перепланировать")).shouldBe(visible).click();
+        $("[data-test-id='success-notification']").shouldBe(visible).
+                shouldHave(exactText("Успешно!\n" + "Встреча успешно запланирована на " + secondMeetingDate));
+    }
+    @Test
     void shouldRequireValidCityIfCityNotIncludeTest() {
         $("[data-test-id='city']").$("[placeholder='Город']").setValue(DataGenerator.generateInvalidCity());
         $("[data-test-id='date']").$("[placeholder='Дата встречи']").setValue(DataGenerator.
@@ -33,7 +61,31 @@ class DeliveryTest {
         $("[data-test-id='city'].input_invalid .input__sub").shouldBe(visible).
                 shouldHave(exactText("Доставка в выбранный город недоступна"));
     }
-
+    @Test
+    void shouldRequireValidDateIfTwoDaysTest() {
+        $("[data-test-id='city']").$("[placeholder='Город']").setValue(DataGenerator.generateValidCity());
+        $("[data-test-id='date']").$("[class='input__control']").setValue(DataGenerator.
+                generateDate(-1));
+        $("[data-test-id= 'name']").$("[name ='name']").setValue(DataGenerator.generateName("ru"));
+        $("[data-test-id='phone']").$("[name='phone']").setValue(DataGenerator.generatePhone());
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id='date'] .input__sub").shouldBe(visible).
+                shouldHave(exactText("Заказ на выбранную дату невозможен"));
+    }
+    @Test
+    void shouldPlaceOrderIfFourDaysPositiveTest() {
+        $("[data-test-id='city']").$("[placeholder='Город']").setValue(DataGenerator.generateValidCity());
+        $("[data-test-id='date']").$("[placeholder='Дата встречи']").setValue(DataGenerator.
+                generateDate(4));
+        $("[data-test-id= 'name']").$("[name ='name']").setValue(DataGenerator.generateName("ru"));
+        $("[data-test-id='phone']").$("[name='phone']").setValue(DataGenerator.generatePhone());
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id='success-notification']").shouldBe(visible).
+                shouldHave(exactText("Успешно!\n" + "Встреча успешно запланирована  на " + DataGenerator.
+                        generateDate(4)));
+    }
     @Test
     void shouldRequireValidNameIfNonCyrillicTest() {
         $("[data-test-id='city']").$("[placeholder='Город']").setValue(DataGenerator.generateValidCity());
@@ -70,7 +122,16 @@ class DeliveryTest {
         $("[data-test-id='city'].input_invalid .input__sub").shouldBe(visible).
                 shouldHave(exactText("Поле обязательно для заполнения"));
     }
-
+    @Test
+    void shouldRequireValidDateIfEmptyDateFieldTest() {
+        $("[data-test-id='city']").$("[placeholder='Город']").setValue(DataGenerator.generateValidCity());
+        $("[data-test-id= 'name']").$("[name ='name']").setValue(DataGenerator.generateName("ru"));
+        $("[data-test-id='phone']").$("[name='phone']").setValue(DataGenerator.generatePhone());
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id='date'] .input__sub").shouldBe(visible).
+                shouldHave(exactText("Неверно введена дата"));
+    }
     @Test
     void shouldRequireValidNameIfEmptyNameFieldTest() {
         $("[data-test-id='city']").$("[placeholder='Город']").setValue(DataGenerator.generateValidCity());
